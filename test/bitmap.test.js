31679e2113f6fe634c9cb9bad711a40caf1c6eb5
Clarefollett <chlaw101@gmail.com>
2017-04-06 14:54:04 -0700

    finally transforming on test

---
+++ test/bitmap.test.js
@@ -0,0 +1,78 @@
+const assert = require('assert');
+const fs = require('fs');
+const invert = require('../lib/invert-colors');
+const grayscale = require('../lib/grayscale-colors');
+const BitmapHeader = require('../lib/bitmap-header');
+const BitmapTransform = require('../lib/bitmap-transform');
+
+let palette = null;
+let noPalette = null;
+
+describe('reads bitmap file header', () => {
+
+    before(done => {
+        fs.readFile('./test/palette-bitmap.bmp', (err, data) => {
+            if(err) done(err);
+            else {
+                palette = data;
+                done();
+            }
+        });
+    });
+
+    before(done => {
+        fs.readFile('./test/non-palette-bitmap.bmp', (err, data) => {
+            if (err) done(err);
+            else {
+                noPalette = data;
+                done();
+            }
+        });
+    });
+
+
+    it('reads the header for no palette', () => {
+        const header = new BitmapHeader(noPalette);
+        assert.equal(header.fileSize, 30054);
+        assert.equal(header.isPaletted, false);
+        assert.equal(header.pixelOffset, 54);
+        assert.equal(header.bitsPerPixel, 24);
+    });
+
+    // it('reads the header for palette', () => {
+    //     const header = new BitmapHeader(palette);
+    //     assert.equal(header.fileSize, 11078);
+    //     assert.equal(header.isPaletted, 256);
+    //     assert.equal(header.pixelOffset, 1078);
+    //     assert.equal(header.bitsPerPixel, 8);
+    // });
+});
+
+describe('transforms to paletted bpm', () => {
+
+    it('inverts all the colors', () => {
+        const bitmap = new BitmapTransform(noPalette);
+        const bmpBuffer = bitmap.transformWithOutPalette(invert);
+
+        // Write the changed buffer/image to a new bitmap file
+        bitmap.write('./test/output.bmp', bmpBuffer, (err) => {
+            if (err) return err;
+            else {
+                // Read and assert the new file
+                fs.readFile('./test/output.bmp', (err, buffer) => {
+                    assert.deepEqual(bmpBuffer, buffer);
+                    done();
+                });
+            }
+        });
+    });
+
+    it('grayscales all the colors', () => {
+
+    });
+    
+    it('', () => {
+
+    });
+
+});
